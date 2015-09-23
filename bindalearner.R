a72b52022d1bb023a4c2675d5c7654d3697d62d9
Dotterbart <dominik.lenk@tu-dortmund.de>
2015-09-23 13:28:41 +0200

    Problem mit den Predictions

--- bindalearner.R
+++ bindalearner.R
@@ -21,9 +21,25 @@ trainLearner.classif.binda <- function(.learner, .task, .subset, .weights = NULL
 
 predictLearner.classif.binda <- function(.learner, .model, .newdata, ...){
   erg <- predict(.model$learner.model, newdata = .newdata, ...)
+#  erg <- predict(.model$learner.model, as.matrix(.newdata), ...)
+  
+#  dat <- getTaskData(.newdata)
+#  taN <- getTaskTargetNames(.newdata)
+#  erg <- predict(.model$learner.model, as.matrix(dat[setdiff(names(dat), taN)]), ...)
   if (learner$predict.type == "response")
     return(erg$class)
   else
     return(erg$posterior)
 }
 
+lrn <- makeLearner("classif.binda")
+tr <- train(lrn, bt)
+
+# Bis dahin funktioniert alles, ich bekomme auch predictions mit
+pr <- predict(tr$learner.model, <<Matrix mit Werten>>)
+
+# Will ich allerdings den mlr-Weg gehen, dann bekomme ich mit
+pr <- predict(tr, <<Task oder Data-Frame>>)
+# immer Fehlermeldungen, egal, ob ich ein "newdata =" davorsetze oder nicht.
+# Ich hab schon so einige Variationen ausprobiert (zwei davon siehe oben), bekomme aber immer Fehler. An der Stelle
+# hänge ich gerade. 
