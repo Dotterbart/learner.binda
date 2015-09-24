b6ca970d09bf3d42d96858d86fa4cb3655c56b76
Dotterbart <dominik.lenk@tu-dortmund.de>
2015-09-24 14:41:50 +0200

    Kommentar detailierter

--- bindalearner.R
+++ bindalearner.R
@@ -19,7 +19,9 @@ trainLearner.classif.binda <- function(.learner, .task, .subset, .weights = NULL
 }
 predictLearner.classif.binda <- function(.learner, .model, .newdata, ...){
   erg <- predict(.model$learner.model, newdata = .newdata, ...)
+  
   # erg <- predict(.model$learner.model, as.matrix(.newdata), ...)
+  
   # dat <- getTaskData(.newdata)
   # taN <- getTaskTargetNames(.newdata)
   # erg <- predict(.model$learner.model, as.matrix(dat[setdiff(names(dat), taN)]), ...)
@@ -28,12 +30,17 @@ predictLearner.classif.binda <- function(.learner, .model, .newdata, ...){
   else
     return(erg$posterior)
 }
+
 lrn <- makeLearner("classif.binda")
 tr <- train(lrn, bt)
-# Bis dahin funktioniert alles, ich bekomme auch predictions mit
-pr <- predict(tr$learner.model, <<Matrix mit Werten>>)
-# Will ich allerdings den mlr-Weg gehen, dann bekomme ich mit
-pr <- predict(tr, <<Task oder Data-Frame>>)
-# immer Fehlermeldungen, egal, ob ich ein "newdata =" davorsetze oder nicht.
-# Ich hab schon so einige Variationen ausprobiert (zwei davon siehe oben), bekomme aber immer Fehler. An der Stelle
-# hänge ich gerade. 
+# Bis dahin funktioniert alles, ich bekomme auch Vorhersagen, wenn ich den klassischen
+# "binda-Weg" gehe, der eine Matrix der neuen Daten will, mittels:
+# pr <- predict(tr$learner.model, <<Matrix mit Werten>>) (**) <-- predict mit Binda-Modell
+
+# Will ich allerdings den "mlr-Weg" gehen, dann bekomme ich mit
+# pr <- predict(tr, <<Task oder Data-Frame>>)
+# immer Fehlermeldungen, die darauf hindeuten, dass die Daten für die neuen Vorhersagen
+# fehlen bzw. in falschem Format vorliegen.
+# Ich habe schon so einige Variationen ausprobiert (zwei davon siehe oben auskommentiert), 
+# bekomme aber immer Fehler, auch bei den beiden auskommentierten Varianten, bei denen
+# nach meinem Verständins eine entsprechende Matrix wie in (**) übergeben werden sollte.
